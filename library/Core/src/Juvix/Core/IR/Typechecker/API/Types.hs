f712b8be2fe27979fcf345537fb72c27863809c5
Drone CI <gianmarco@heliax.dev>
2021-09-27 11:25:42 +0100

    Fix some compile errors

--- library/Core/src/Juvix/Core/IR/Typechecker/API/Types.hs
+++ library/Core/src/Juvix/Core/IR/Typechecker/API/Types.hs
@@ -7,7 +7,7 @@ import Juvix.Core.IR.Typechecker.Env as Env
 import qualified Juvix.Core.Base.Types as Core
 import qualified Juvix.Core.IR.Evaluator as Eval
 import qualified Juvix.Core.IR.Types as IR
-import qualified Juvix.Core.Typechecker.API.History as Hist
+import qualified Juvix.Core.IR.Typechecker.API.History as Hist
 -- import qualifief Juvix.Core.Unification as Uni
 
 -- | This API is based on Idris's v1 proof state
@@ -19,29 +19,29 @@ data Error primTy primVal a
   | HolesErr         a -- Uni.HolesErr
   | LocalContextErr  a -- Uni.TermContextErr
   | GlobalContextErr a -- Core.GlobalErr
-  | MetaProgErr      MetaProgErr
-  | PrimErr          (Core.PrimErr primTy primVal)
+  | MetaProgErr      MetaProgError
+  -- | PrimErr          (Core.PrimErr primTy primVal)
 
  -- `a` is a placeholder for the actual types
-data ProofState primTy primVal a = Env
+data ProofState ext primTy primVal a b = Env
   { -- captures all changes to proof term
     history        :: Hist.History a b,
 
     -- unsolved unification problems
     -- requires unification
     -- TODO: check with Andy how the unification goal works
-    proofGoal      :: a -- Uni.UnificationGoal primTy primVal,
+    proofGoal      :: a, -- Uni.UnificationGoal primTy primVal,
 
     -- current proof
     -- TODO: check with Andy how to deal with bidirectional typecheking here
-    proofTerm      :: Core.Term primTy primVal,
+    proofTerm      :: Core.Term ext primTy primVal,
 
      -- global context with all types
-    globals        :: Core.RawGlobal primTy primVal,
+    globals        :: Core.RawGlobal ext primTy primVal,
 
     -- local context of `proofTerm`
     -- requires unification
-    locals         :: a -- Uni.TermConxtext primTy primVal,
+    locals         :: a, -- Uni.TermConxtext primTy primVal,
 
     -- queue of current holes and guesses
     -- requires unification
@@ -49,24 +49,24 @@ data ProofState primTy primVal a = Env
   }
   deriving (Generic)
 
-type ProofStateA primTy primVal2 =
+type ProofStateA primTy primVal a b =
   ExceptT
-    (Error primTy primVal)
-    (State (ProofState primTy primVal))
+    (Error primTy primVal a)
+    (State (ProofState IR.T primTy primVal a b))
 
 newtype ProofStateT primTy primVal a b
   = ProofStateT (ProofStateA primTy primVal a b)
   deriving (Functor, Applicative, Monad)
   deriving
-    ( HasState  "history" (History a b),
-      HasSink   "history" (History a b),
-      HasSource "history" (History a b)
+    ( HasState  "history" (Hist.History a b),
+      HasSink   "history" (Hist.History a b),
+      HasSource "history" (Hist.History a b)
     )
     via StateField "history" (ProofStateA primTy primVal a b)
   deriving
-    ( HasState  "globals" (History primTy primVal),
-      HasSink   "globals" (History primTy primVal),
-      HasSource "globals" (History primTy primVal)
+    ( HasState  "globals" (Hist.History primTy primVal),
+      HasSink   "globals" (Hist.History primTy primVal),
+      HasSource "globals" (Hist.History primTy primVal)
     )
     via StateField "history" (ProofStateA primTy primVal a b)
 
@@ -98,5 +98,5 @@ newtype ProofStateT primTy primVal a b
     (HasThrow "proofError" (Error primTy1 primVal1 a))
     via MonadError (ProofStateA primTy primVal)
 
-data MetaProgErr
-  = MetaProgErr -- not sure what errors can happen here, but will leave a placeholder
+data MetaProgError
+  = MetaProgError -- not sure what errors can happen here, but will leave a placeholder
