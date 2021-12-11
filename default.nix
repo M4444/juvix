6c278273ccd5bb541de4d50ea61135da2d1ff2d3
Samuli Thomasson <samuli.thomasson@pm.me>
2021-12-11 04:10:11 +0100

    Added Nix build via haskell.nix
    This makes it possible to build all the Haskell packages as Nix
    expressions. Also makes cross-compiling for x86_64-linux-musl (to
    produce highly portable statically linked executables) very easy.

---
+++ default.nix
@@ -0,0 +1,60 @@
+{ compiler-nix-name ? "ghc8107" # Should match the one in stack.yaml
+, doHaddock ? doHoogle # hoogle requires haddock
+, doHoogle ? false
+, materialized ? if builtins.pathExists nix/materialized then nix/materialized else null
+, checkMaterialization ? false
+, ...
+}:
+let
+  overlays = [
+    # So that haskell.nix knows to find the numa library (when -fnuma)
+    (self: super: { numa = self.numactl; })
+  ];
+
+  # Loads the stack project (stack.yaml) using the haskell.nix infrastructure.
+  project' = { lib, stdenv, haskell-nix, buildPackages }:
+    haskell-nix.stackProject' {
+      # The "cleanGit" filters out untracked files. You don't need to
+      # stage changes, it will use the files in the worktree, but you do
+      # need to `git add` new files so that they become tracked.
+      src = haskell-nix.haskellLib.cleanGit {
+        name = "juvix";
+        src = ./.;
+      };
+      inherit compiler-nix-name;
+      modules = [{ inherit doHaddock doHoogle; }];
+      # For git sources in stack.yaml
+      sha256map = import ./nix/stack-sha256map.nix;
+      # Materializing speeds up the evaluation of nix expressions.
+      inherit materialized checkMaterialization;
+    };
+
+  sources = import ./nix/sources.nix { };
+  haskellNix = import sources.haskellNix { };
+  # Import nixpkgs with customizations specified by haskell.Nix + our own overlays.
+  nixpkgsSrc = haskellNix.sources.nixpkgs-unstable;
+  nixpkgsArgs = haskellNix.nixpkgsArgs;
+  nixpkgsOverlays = nixpkgsArgs.overlays ++ overlays;
+  pkgs = import nixpkgsSrc (nixpkgsArgs // { overlays = nixpkgsOverlays; });
+  lib = pkgs.lib;
+  haskellLib = pkgs.haskell-nix.haskellLib;
+
+  project = pkgs.callPackage project' {
+    inherit (pkgs) buildPackages;
+  };
+in
+
+rec {
+  # The project generated from the stack.yaml via haskell.nix
+  inherit project;
+  # All local Haskell packages.
+  local = haskellLib.selectLocalPackages project.hsPkgs;
+  # The same project except everything is linked statically (musl).
+  static.project = project.projectCross.musl64;
+  # All local Haskell packages linked statically.
+  static.local = haskellLib.selectLocalPackages static.project.hsPkgs;
+  # some convenience exports
+  inherit (project) pkgs;
+}
+  # Add shorthands for local exes, tests and benchmarks
+  // lib.genAttrs [ "exes" "tests" "benchmarks" ] (component: haskellLib.collectComponents component haskellLib.isLocalPackage project.hsPkgs)
---
+++ shell.nix
@@ -0,0 +1,18 @@
+{...}@args:
+let
+  # Build haddock and hoogle indices, so that e.g. hoogle works.
+  defaultArgs = { doHaddock = true; doHoogle = true; };
+  project = (import ./. (defaultArgs // args)).project;
+  hsLib = project.pkgs.haskell-nix.haskellLib;
+in
+  project.shellFor {
+    # There are various settings you could add here. See:
+    # https://input-output-hk.github.io/haskell.nix/reference/library.html#shellfor
+
+    # Workaround for a bug in haskell.nix:
+    # https://github.com/input-output-hk/haskell.nix/issues/590#issuecomment-712702992
+    additional = ps: builtins.attrValues (hsLib.selectLocalPackages ps);
+
+    # Instruct cabal to not deviate from the package set provided by nix.
+    exactDeps = true;
+  }
