f672e0db96ba4858200d9321d882c7b67aa931e5
Drone CI <centelles.alberto@gmail.com>
2021-12-13 12:30:11 +0000

    Return correct patterns

--- library/Core/src/Juvix/Core/CaseTree.hs
+++ library/Core/src/Juvix/Core/CaseTree.hs
@@ -1,12 +1,12 @@
 module Juvix.Core.CaseTree where
 
+import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Map as Map
+import qualified Juvix.Core.Base as Core
+import qualified Juvix.Core.Base.TransformExt.OnlyExts as OnlyExts
+import qualified Juvix.Core.IR.Types as IR
 import Juvix.Library
 import qualified Juvix.Library.NameSymbol as NameSymbol
-import qualified Juvix.Core.Base as Core
-import qualified Juvix.Core.IR.Types as IR
-import qualified Data.List.NonEmpty as NonEmpty
-import qualified Juvix.Core.Base.TransformExt.OnlyExts as OnlyExts
 
 -- Inputs of the algorithm
 
@@ -19,33 +19,34 @@ import qualified Juvix.Core.Base.TransformExt.OnlyExts as OnlyExts
 
 -- The outputs of the algorithm are a signature Sigma' extending Sigma with new clauses and a well-typed case tree Q such that `Simga; T |- f q := Q : C ~> Sigma'`
 
-
 -- We represent the user input P to the algorithm as an ordered list of partially decomposed clauses, called a left-hand side problem. Each partially decomposed claused is of the form:
 -- [E]q -> rhs, where E is an (unordered) set of constraints
 
-clausesToCaseTree 
-    :: forall primTy primVal. Core.RawFunction IR.T primTy primVal
-    -> Core.RawFunctionCase IR.T primTy primVal
-clausesToCaseTree (Core.RawFunction name usage ty clauses) 
-  = Core.RawFunctionCase name usage ty (go 0 clauses Nothing)
-  where 
+clausesToCaseTree ::
+  forall primTy primVal.
+  Core.RawFunction IR.T primTy primVal ->
+  Core.RawFunctionCase IR.T primTy primVal
+clausesToCaseTree (Core.RawFunction name usage ty clauses) =
+  Core.RawFunctionCase name usage ty (go 0 clauses Nothing)
+  where
     go :: Int -> NonEmpty (Core.RawFunClause IR.T primTy primVal) -> Maybe (Core.Term IR.T primTy primVal) -> Core.CaseTree IR.T primTy primVal
     go idx clauses bodyM =
-        let nextArgPats = nextArgPatterns clauses
-            nextClauses = elimNextArgPatterns clauses
-            zipped = zip [0..] nextArgPats
-        in case (zipped, bodyM) of
-          ([], Just body) -> IR.Done [] body
-          ([], Nothing) -> IR.Fail []
-          _ -> IR.Case (Core.Arg idx) $ f <$> zipped
-            where 
-              f :: (Int, Core.Pattern IR.T primTy primVal) -> Core.Branch IR.T primTy primVal
-              f (idx, pat) = let cl = clauses NonEmpty.!! idx 
-                              in IR.Branch pat (go (idx + 1) nextClauses (Just $ Core.rawClauseBody cl))
+      let nextArgPats = nextArgPatterns clauses
+          nextClauses = elimNextArgPatterns clauses
+          zipped = zip [0 ..] nextArgPats
+       in case (zipped, bodyM) of
+            ([], Just body) -> IR.Done [] body
+            ([], Nothing) -> IR.Fail []
+            _ -> IR.Case (Core.Arg idx) $ f <$> zipped
+              where
+                f :: (Int, Core.Pattern IR.T primTy primVal) -> Core.Branch IR.T primTy primVal
+                f (idx, pat) =
+                  let cl = clauses NonEmpty.!! idx
+                   in IR.Branch pat (go (idx + 1) nextClauses (Just $ Core.rawClauseBody cl))
 
 elimNextArgPatterns :: NonEmpty (Core.RawFunClause ext primTy primVal) -> NonEmpty (Core.RawFunClause ext primTy primVal)
-elimNextArgPatterns (Core.RawFunClause tel (pat : pats) rhs catchAll :| clauses) 
-  = Core.RawFunClause tel pats rhs catchAll :| (reverse $ go clauses [])
+elimNextArgPatterns (Core.RawFunClause tel (pat : pats) rhs catchAll :| clauses) =
+  Core.RawFunClause tel pats rhs catchAll :| (reverse $ go clauses [])
   where
     go [] acc = acc
     go (Core.RawFunClause tel (pat : pats) rhs catchAll : clauses) acc =
@@ -54,16 +55,15 @@ elimNextArgPatterns (Core.RawFunClause tel (pat : pats) rhs catchAll :| clauses)
 elimNextArgPatterns c = c
 
 nextArgPatterns :: NonEmpty (Core.RawFunClause ext primTy primVal) -> [Core.Pattern ext primTy primVal]
-nextArgPatterns (Core.RawFunClause tel (pat : pats) rhs catchAll :| clauses) 
-  = reverse $ go clauses [pat]  
-    where
-      go [] acc = reverse acc
-      go (Core.RawFunClause _ (pat : pats) rhs _ : clauses) acc =
-            go clauses (pat : acc)
-nextArgPatterns (Core.RawFunClause tel [] rhs catchAll :| clauses) 
-  = []
+nextArgPatterns (Core.RawFunClause tel (pat : pats) rhs catchAll :| clauses) =
+  go clauses [pat]
+  where
+    go [] acc = reverse acc
+    go (Core.RawFunClause _ (pat : pats) rhs _ : clauses) acc =
+      go clauses (pat : acc)
+nextArgPatterns (Core.RawFunClause tel [] rhs catchAll :| clauses) =
+  []
 
 nextPatternSig :: Core.Term ext primTy primVal -> Core.Term ext primTy primVal
 nextPatternSig (Core.Pi _ pat _ _) = pat
 nextPatternSig _ = panic "Invalid nextPatternSig"
-
