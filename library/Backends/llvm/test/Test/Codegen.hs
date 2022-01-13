a2b1ea8bb4b42553a564a259b9b049b1f98cea30
Terence Rokop <rokopt@sharpears.net>
2022-01-13 13:25:46 -0800

    Add HUnit test for LLVM code generation

--- library/Backends/llvm/test/Test/Codegen.hs
+++ library/Backends/llvm/test/Test/Codegen.hs
@@ -14,6 +14,7 @@ import qualified LLVM.AST as AST
 import qualified LLVM.ExecutionEngine as Engine
 import qualified LLVM.Internal.Context as LLVMContext
 import qualified LLVM.Module as LLVMModule
+import qualified LLVM.Pretty as Pretty
 import Test.Tasty (TestName, TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 import qualified Prelude as P
@@ -33,20 +34,62 @@ runIntModule mod =
             Just f -> runIntFun f
             Nothing -> P.error "no main function found"
 
+llvmIntType :: Word32 -> PassTypes.TypeLLVM
+llvmIntType size = PassTypes.PrimTy $ Primitive.PrimTy $ AST.IntegerType {typeBits = size}
+
+llvmIntVal :: Integer -> PassTypes.TermLLVM
+llvmIntVal = PassTypes.Prim . Primitive.LitInt
+
+llvmAnnotatedTerm :: PassTypes.TypeLLVM -> PassTypes.TermLLVM -> PassTypes.Annotated PassTypes.TermLLVM
+llvmAnnotatedTerm ty term = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = ty, PassTypes.term = term}
+
 top :: TestTree
 top = testGroup "LLVM Codegen tests" tests
 
 tests :: [TestTree]
 tests =
-  [trivialLLVMCodegenTest]
+  [trivialLLVMCodegenClosureTest, trivialLLVMCodegenAlgebraicTest]
 
-trivialLLVMCodegenTest :: TestTree
-trivialLLVMCodegenTest = testCase "Trivial LLVM codegen test" $ do
+trivialLLVMCodegenClosureTest :: TestTree
+trivialLLVMCodegenClosureTest = testCase "Trivial LLVM codegen test with closure type" $ do
   let testVal = 42
-      ty = PassTypes.PrimTy $ Primitive.PrimTy $ AST.IntegerType {typeBits = 8}
+      ty = llvmIntType 8
       term = PassTypes.Prim (Primitive.LitInt testVal)
       compiled =
         fromRight (P.error "trivial test compilation failed") $
           Compilation.termLLVMToModule PassTypes.Ann {usage = Usage.SAny, annTy = ty, term = term}
   output <- runIntModule compiled
-  output @?= fromIntegral testVal
+  let casted :: Int8 = fromIntegral output
+  casted @?= fromIntegral testVal
+
+trivialLLVMCodegenAlgebraicTest :: TestTree
+trivialLLVMCodegenAlgebraicTest = testCase "Trivial LLVM codegen test with algebraic types" $ do
+  let test8aVal = 43
+      test8bVal = 44
+      test16Val = 45
+      int8Ty = llvmIntType 8
+      int16Ty = llvmIntType 16
+      recordTypeName = "testRecordType"
+      field8aName = "testFieldInt8a"
+      field8bName = "testFieldInt8b"
+      field16Name = "testFieldInt16"
+      testRecordTy = (recordTypeName, [(field8aName, int8Ty), (field8bName, int8Ty), (field16Name, int16Ty)])
+      internalTerm8a = PassTypes.Prim $ Primitive.LitInt test8aVal
+      annotatedTerm8a = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = int8Ty, PassTypes.term = internalTerm8a}
+      internalTerm8b = PassTypes.Prim $ Primitive.LitInt test8bVal
+      annotatedTerm8b = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = int8Ty, PassTypes.term = internalTerm8b}
+      internalTerm16 = PassTypes.Prim $ Primitive.LitInt test16Val
+      annotatedTerm16 = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = int16Ty, PassTypes.term = internalTerm16}
+      testRecordTerm = PassTypes.RecordM (recordTypeName, [annotatedTerm8a, annotatedTerm8b, annotatedTerm16])
+      annotatedRecordTerm = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = PassTypes.RecordType recordTypeName, PassTypes.term = testRecordTerm}
+      testFieldTerm = PassTypes.FieldM (recordTypeName, field8bName, annotatedRecordTerm)
+      internalAnnotatedTerm = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = int8Ty, PassTypes.term = testFieldTerm}
+      term = PassTypes.ScopedRecordDeclM testRecordTy internalAnnotatedTerm
+      annotatedTerm = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = int8Ty, PassTypes.term = term}
+      compiled =
+        fromRight (P.error "trivial test compilation failed") $
+          Compilation.termLLVMToModule annotatedTerm
+  putStrLn $ "LLVM AST: " <> Pretty.ppllvm compiled
+  output <- runIntModule compiled
+  let casted :: Int8 = fromIntegral output
+  casted @?= fromIntegral test8bVal
