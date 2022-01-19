cb404b4603d7b83561c127c2ac1e39df0ac8ae0d
Terence Rokop <rokopt@sharpears.net>
2022-01-18 18:25:26 -0800

    Test scoping of record definitions

--- library/Backends/llvm/test/Test/Codegen.hs
+++ library/Backends/llvm/test/Test/Codegen.hs
@@ -11,6 +11,7 @@ import qualified Juvix.Core.Erased.Ann as ErasedAnn
 import Juvix.Library
 import qualified Juvix.Library.Usage as Usage
 import qualified LLVM.AST as AST
+import qualified LLVM.AST.Type as ASTTypes
 import qualified LLVM.ExecutionEngine as Engine
 import qualified LLVM.Internal.Context as LLVMContext
 import qualified LLVM.Module as LLVMModule
@@ -43,12 +44,30 @@ llvmIntVal = PassTypes.Prim . Primitive.LitInt
 llvmAnnotatedTerm :: PassTypes.TypeLLVM -> PassTypes.TermLLVM -> PassTypes.Annotated PassTypes.TermLLVM
 llvmAnnotatedTerm ty term = PassTypes.Ann {PassTypes.usage = Usage.SAny, PassTypes.annTy = ty, PassTypes.term = term}
 
+annotatedBinOp  ::
+  ASTTypes.Type ->
+  Primitive.RawPrimVal ->
+  PassTypes.Annotated PassTypes.TermLLVM
+annotatedBinOp ty op = llvmAnnotatedTerm (PassTypes.PrimTy $ Primitive.PrimTy ty) $
+  PassTypes.Prim op
+
+annotatedBinOpApp ::
+  PassTypes.TypeLLVM ->
+  PassTypes.Annotated PassTypes.TermLLVM ->
+  PassTypes.Annotated PassTypes.TermLLVM ->
+  PassTypes.Annotated PassTypes.TermLLVM ->
+  PassTypes.Annotated PassTypes.TermLLVM
+annotatedBinOpApp ty op term term' = llvmAnnotatedTerm ty (PassTypes.AppM op [term, term'])
+
 top :: TestTree
 top = testGroup "LLVM Codegen tests" tests
 
 tests :: [TestTree]
 tests =
-  [trivialLLVMCodegenClosureTest, trivialLLVMCodegenAlgebraicTest]
+  [ trivialLLVMCodegenClosureTest,
+    trivialLLVMCodegenAlgebraicTest,
+    llvmRecordNameShadowingTest
+  ]
 
 trivialLLVMCodegenClosureTest :: TestTree
 trivialLLVMCodegenClosureTest = testCase "Trivial LLVM codegen test with closure type" $ do
@@ -87,8 +106,58 @@ trivialLLVMCodegenAlgebraicTest = testCase "Trivial LLVM codegen test with algeb
       term = PassTypes.ScopedRecordDeclM testRecordTy internalAnnotatedTerm
       annotatedTerm = llvmAnnotatedTerm int8Ty term
       compiled =
-        fromRight (P.error "trivial test compilation failed") $
+        fromRight (P.error "LLVM algebraic types test compilation failed") $
           Compilation.termLLVMToModule annotatedTerm
   output <- runIntModule compiled
   let casted :: Int8 = fromIntegral output
   casted @?= fromIntegral test8bVal
+
+llvmRecordNameShadowingTest :: TestTree
+llvmRecordNameShadowingTest = testCase "LLVM codegen test with record name shadowing" $ do
+  let shadowedName = "shadowedRec"
+      shadowedFieldName = "shadowedField"
+      shadowingFieldName = "shadowingField"
+      intTy = llvmIntType 32
+      addOp = annotatedBinOp ASTTypes.i32 Primitive.Add
+      mulOp = annotatedBinOp ASTTypes.i32 Primitive.Mul
+      shadowingVal = 1
+      shadowedValLeft = 2
+      shadowedValRight = 4
+      shadowedTy = (shadowedName, [(shadowedFieldName, intTy)])
+      shadowingTy = (shadowedName, [(shadowingFieldName, intTy)])
+      shadowingInt = llvmIntVal shadowingVal
+      shadowedIntLeft = llvmIntVal shadowedValLeft
+      shadowedIntRight = llvmIntVal shadowedValRight
+      shadowingAnnotated = llvmAnnotatedTerm intTy shadowingInt
+      shadowedAnnotatedLeft = llvmAnnotatedTerm intTy shadowedIntLeft
+      shadowedAnnotatedRight = llvmAnnotatedTerm intTy shadowedIntRight
+      shadowingRecord = PassTypes.RecordM (shadowedName, [shadowingAnnotated])
+      annotatedShadowingRecord =
+        llvmAnnotatedTerm (PassTypes.RecordType shadowedName) shadowingRecord
+      shadowingField =
+        PassTypes.FieldM (shadowedName, shadowingFieldName, annotatedShadowingRecord)
+      annotatedShadowingField = llvmAnnotatedTerm intTy shadowingField
+      shadowingTerm = PassTypes.ScopedRecordDeclM shadowingTy annotatedShadowingField
+      annotatedShadowingTerm = llvmAnnotatedTerm intTy shadowingTerm
+      shadowedRecordLeft = PassTypes.RecordM (shadowedName, [shadowedAnnotatedLeft])
+      shadowedRecordRight = PassTypes.RecordM (shadowedName, [shadowedAnnotatedRight])
+      annotatedShadowedRecordLeft =
+        llvmAnnotatedTerm (PassTypes.RecordType shadowedName) shadowedRecordLeft
+      annotatedShadowedRecordRight =
+        llvmAnnotatedTerm (PassTypes.RecordType shadowedName) shadowedRecordRight
+      shadowedFieldLeft =
+        PassTypes.FieldM (shadowedName, shadowedFieldName, annotatedShadowedRecordLeft)
+      shadowedFieldRight =
+        PassTypes.FieldM (shadowedName, shadowedFieldName, annotatedShadowedRecordRight)
+      annotatedShadowedFieldLeft = llvmAnnotatedTerm intTy shadowedFieldLeft
+      annotatedShadowedFieldRight = llvmAnnotatedTerm intTy shadowedFieldRight
+      mulTerm = annotatedBinOpApp intTy mulOp annotatedShadowingTerm annotatedShadowedFieldRight
+      addTerm = annotatedBinOpApp intTy addOp annotatedShadowedFieldLeft mulTerm
+      shadowedTerm = PassTypes.ScopedRecordDeclM shadowedTy addTerm
+      annotatedShadowedTerm = llvmAnnotatedTerm intTy shadowedTerm
+      compiled =
+        fromRight (P.error "trivial test compilation failed") $
+          Compilation.termLLVMToModule annotatedShadowedTerm
+  output <- runIntModule compiled
+  let casted :: Int32 = fromIntegral output
+  casted @?= fromIntegral (shadowedValLeft + (shadowingVal * shadowedValRight))
