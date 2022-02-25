21202a163bdf22daec4910f1ee7c4f0c86c215cf
Terence Rokop <rokopt@sharpears.net>
2022-02-24 18:51:26 -0800

    Erasure of Categorial terms
    (Along with a bit more pretty-printing and serialization.)

--- library/Core/src/Juvix/Core/Erased/Base/Types.hs
+++ library/Core/src/Juvix/Core/Erased/Base/Types.hs
@@ -4,6 +4,7 @@ module Juvix.Core.Erased.Base.Types where
 
 import Extensible
 import Juvix.Core.Base.Types (Universe)
+import qualified Juvix.Core.Categorial as Categorial
 import Juvix.Library hiding (Type)
 import qualified Juvix.Library.HashMap as Map
 import qualified Juvix.Library.NameSymbol as NameSymbol
@@ -23,6 +24,7 @@ extensible
       | CatCoproductIntroLeft (Term primVal)
       | CatCoproductIntroRight (Term primVal)
       | CatCoproductElim (Term primVal) (Term primVal) (Term primVal) (Term primVal) (Term primVal)
+      | CategorialTerm (Categorial.Term (Term primVal))
       | Unit
       | Let NameSymbol.T (Term primVal) (Term primVal)
       | App (Term primVal) (Term primVal)
@@ -38,6 +40,7 @@ extensible
       | Sig Usage.T (Type primTy) (Type primTy)
       | CatProduct (Type primTy) (Type primTy)
       | CatCoproduct (Type primTy) (Type primTy)
+      | CategorialType Usage.T
       deriving (Show, Eq, Generic)
 
     type TypeAssignment primTy = Map.T NameSymbol.T (Type primTy)
