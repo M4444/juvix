47ebf1f0733fc29fc5a311b68f242ca96bce594c
Paul Cadman <git@paulcadman.dev>
2022-02-25 12:00:00 +0000

    Moves Berlin desugar passes to a new module

--- library/Translate/src/Juvix/Desugar/Env.hs
+++ library/Translate/src/Juvix/Desugar/Env.hs
@@ -2,6 +2,8 @@ module Juvix.Desugar.Env where
 
 import Control.Lens hiding ((|>))
 import qualified Data.List.NonEmpty as NonEmpty
+import qualified Juvix.BerlinPasses.Desugar as Desugar
+import qualified Juvix.BerlinPasses.Environment as Env
 import qualified Juvix.BerlinPipeline.Automation as Automation
 import qualified Juvix.BerlinPipeline.CircularList as CircularList
 import qualified Juvix.BerlinPipeline.Env as Pipeline.Env
@@ -15,8 +17,6 @@ import qualified Juvix.Context.Traversal as Context
 import qualified Juvix.Contextify as Contextify
 import qualified Juvix.Contextify.Passes as Passes
 import qualified Juvix.Contextify.ToContext.ResolveOpenInfo as ResolveOpen
-import qualified Juvix.Desugar.Passes as Desugar.Passes
-import qualified Juvix.BerlinPasses.Environment as Env
 import Juvix.Library hiding (trace)
 import qualified Juvix.Library.NameSymbol as NameSymbol
 import qualified Juvix.Library.Trace as Trace
@@ -72,16 +72,16 @@ eval = do
 
 desugarPasses :: CircularList.T Step.Named
 desugarPasses =
-  [ headerPass,
-    moduleLetPass,
-    modulePass,
-    condPass,
-    ifPass,
-    letPass,
-    multipleDefunPass,
-    combineSigPass,
-    removePunnedRecordsPass,
-    handlerPass
+  [ Desugar.headerPass,
+    Desugar.moduleLetPass,
+    Desugar.modulePass,
+    Desugar.condPass,
+    Desugar.ifPass,
+    Desugar.letPass,
+    Desugar.multipleDefunPass,
+    Desugar.combineSigPass,
+    Desugar.removePunnedRecordsPass,
+    Desugar.handlerPass
   ]
     >>| CircularList.init
     |> Pipeline.Env.defPipelineGroup "DesugarPasses"
@@ -95,91 +95,6 @@ contextPasses =
   ]
     >>| CircularList.init
     |> Pipeline.Env.defPipelineGroup "ContextPasses"
-
-condPass :: Step.Named
-condPass =
-  (Trace.withScope "Desugar.cond-runner" [] . Automation.simplify condTrans)
-    |> Automation.runSimplifiedPass
-    |> Step.T
-    |> Step.namePass "Desugar.cond-to-if"
-
-condTrans :: Automation.SimplifiedPassArgument -> Env.MinimalMIO Automation.Job
-condTrans simplify = do
-  Trace.withScope "Desugar.condTrans" [show (simplify ^. current)] $ do
-    condTransform (simplify ^. current)
-      >>| (`Automation.ProcessNoEnv` [])
-      >>| Automation.ProcessJob
-
--- | @condTransform@ - CondTransform turns the cond form of the fronted
--- language into a series of ifs
--- - BNF input form:
---   + (:cond (pred-1 result-1) … (pred-n result-n))
--- - BNF output form:
---   + (if pred-1 result-1 (if pred-2 result-2 (… (if pred-n result-n))))
--- condTransform :: Sexp.T -> Sexp.T
-condTransform :: (MonadIO m, Meta.HasMeta m) => Sexp.T -> m Sexp.T
-condTransform xs =
-  Trace.withScope "Desugar.condTransform" [show xs] $ do
-    Sexp.traversePredStar xs (== Structure.nameCond) condToIf
-  where
-    condToIf sexp@(Sexp.Atom atom Sexp.:> _) recur
-      | Just cond <- Structure.toCond sexp,
-        Just last <- lastMay (cond ^. entailments) =
-        let acc =
-              Structure.IfNoElse (last ^. predicate) (last ^. answer)
-                |> Structure.fromIfNoElse
-         in foldr generation acc (initSafe (cond ^. entailments))
-              |> Sexp.addMetaToCar atom
-              |> recur
-    condToIf _ _ = Env.throw $ Env.MalformedData "cond is in an invalid format"
-    --
-    generation predAns acc =
-      Structure.If (predAns ^. predicate) (predAns ^. answer) acc
-        |> Structure.fromIf
-
-modulePass :: Step.Named
-modulePass = desugarPass Desugar.Passes.moduleTransform "module"
-
-moduleLetPass :: Step.Named
-moduleLetPass = desugarPass Desugar.Passes.moduleLetTransform "moduleLet"
-
-ifPass :: Step.Named
-ifPass = desugarPass Desugar.Passes.ifTransform "if"
-
-letPass :: Step.Named
-letPass = desugarPass Desugar.Passes.multipleTransLet "let"
-
-removePunnedRecordsPass :: Step.Named
-removePunnedRecordsPass =
-  desugarPass Desugar.Passes.removePunnedRecords "removePunnedRecords"
-
-handlerPass :: Step.Named
-handlerPass = desugarPass Desugar.Passes.handlerTransform "handlerTransform"
-
-desugarPass ::
-  (Sexp.T -> Sexp.T) ->
-  NameSymbol.T ->
-  Step.Named
-desugarPass trans name =
-  (Trace.withScope scopeName [] . Automation.simplify f)
-    |> Automation.runSimplifiedPass
-    |> Step.T
-    |> Step.namePass passName
-  where
-    f = desugarTrans trans name
-    passName = "Desugar." <> name
-    scopeName = passName <> "-runner"
-
-desugarTrans ::
-  (Sexp.T -> Sexp.T) ->
-  NameSymbol.T ->
-  Automation.SimplifiedPassArgument ->
-  Env.MinimalMIO Automation.Job
-desugarTrans trans name simplify = do
-  Trace.withScope ("Desugar." <> name) [show (simplify ^. current)] $ do
-    (pure . trans) (simplify ^. current)
-      >>| (`Automation.ProcessNoEnv` [])
-      >>| Automation.ProcessJob
 
 --------------------------------------------------------------------------------
 -- InPackage
@@ -201,50 +116,6 @@ inPackageTrans arg = case arg ^. current of
       set context newCtx arg |> pure
     noOp = arg |> pure
     ctx = arg ^. context
-
---------------------------------------------------------------------------------
--- Header
---------------------------------------------------------------------------------
-
-injectCurrentPackageContext :: Pipeline.CIn -> Pipeline.CIn
-injectCurrentPackageContext cin =
-  over (languageData . currentExp) f cin
-  where
-    f =
-      (cin ^. languageData . context)
-        |> Context.currentName
-        |> NameSymbol.cons Context.topLevelName
-        |> Structure.InPackage
-        |> Sexp.serialize
-        |> Pipeline.Sexp
-        |> (:)
-
-headerPass :: Step.Named
-headerPass =
-  (Trace.withScope "Desugar.header-runner" [] . Automation.simplify headerTrans)
-    |> (\f -> Automation.runSimplifiedPass f . injectCurrentPackageContext)
-    |> Step.T
-    |> Step.namePass "Desugar.header"
-
-headerTrans :: Automation.SimplifiedPassArgument -> Env.MinimalMIO Automation.Job
-headerTrans simplify =
-  Trace.withScope "Desugar.headerTrans" [show (simplify ^. current)] $
-    headerTransform (simplify ^. current)
-      >>| (\(h, sexps) -> Automation.ProcessNoEnv h (fmap f sexps))
-      >>| Automation.ProcessJob
-  where
-    f s = (Automation.Current, s)
-
--- TODO: Make this recursive?
-headerTransform :: (Meta.HasMeta m) => Sexp.T -> m (Sexp.T, [Sexp.T])
-headerTransform sexp = case Structure.toHeader sexp of
-  Nothing -> (sexp, []) |> pure
-  Just (Structure.Header name xs) ->
-    case Sexp.toList @Maybe xs of
-      Just sexps ->
-        (Sexp.serialize (Structure.InPackage (Context.topLevelName <> name)), sexps)
-          |> pure
-      Nothing -> Env.throw $ Env.MalformedData "header is in an invalid format"
 
 --------------------------------------------------------------------------------
 -- Resolve Modules
@@ -401,49 +272,6 @@ recordDeclaration pa =
                 _newForms = []
               }
         }
-
---------------------------------------------------------------------------------
--- Multiple Defun
---------------------------------------------------------------------------------
-
-multipleDefunPass :: Step.Named
-multipleDefunPass =
-  (pure . transMultiSexpNoContext Desugar.Passes.multipleTransDefun)
-    |> Step.T
-    |> Step.namePass "Context.multipleDefun"
-
---------------------------------------------------------------------------------
--- Combine Sig
---------------------------------------------------------------------------------
-
-combineSigPass :: Step.Named
-combineSigPass =
-  pure . transMultiSexpNoContext Desugar.Passes.combineSig
-    |> Step.T
-    |> Step.namePass "Context.combineSig"
-
-transMultiSexpNoContext ::
-  ([Sexp.T] -> [Sexp.T]) ->
-  Pipeline.CIn ->
-  Pipeline.COut Pipeline.WorkingEnv
-transMultiSexpNoContext trans cin =
-  Pipeline.Success meta newWenv
-  where
-    wenv = cin ^. languageData
-    sexps = (wenv ^. currentExp) >>= f
-    meta = cin ^. surroundingData . metaInfo
-
-    newSexps = trans sexps >>| Pipeline.Sexp
-
-    newWenv =
-      Pipeline.WorkingEnv
-        { _currentExp = newSexps,
-          _context = wenv ^. context
-        }
-
-    -- This pass runs before Context is initialized
-    f (Pipeline.InContext _) = []
-    f (Pipeline.Sexp sexp) = [sexp]
 
 --------------------------------------------------------------------------------
 -- Contextify
