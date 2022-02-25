31d73ebd7bfec0556c525c1175afeb97d5dd9485
Terence Rokop <rokopt@sharpears.net>
2022-02-24 18:31:40 -0800

    Remove unneeded import

--- library/Core/src/Juvix/Core/IR/Evaluator/SubstV.hs
+++ library/Core/src/Juvix/Core/IR/Evaluator/SubstV.hs
@@ -11,7 +11,6 @@ module Juvix.Core.IR.Evaluator.SubstV
   )
 where
 
-import Data.Foldable (foldr1) -- on NonEmpty
 import qualified Juvix.Core.Application as App
 import qualified Juvix.Core.Base.Types as Core
 import Juvix.Core.IR.Evaluator.Types
